7cccb17526ff779794f4fe47f23d4ac53c14597c
hedutrack <hedutrack@gmail.com>
2015-09-25 16:14:37 -0400

    Testing with github

--- app/src/main/java/com/hayseed/mysunshine/MainActivityFragment.java
+++ app/src/main/java/com/hayseed/mysunshine/MainActivityFragment.java
@@ -21,7 +21,6 @@ public class MainActivityFragment extends Fragment
     {
     }
 
-
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container,
                               Bundle savedInstanceState)
